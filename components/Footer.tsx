c00c1e34d8088f905effe21c30990df733da5384
Abhirup Saha <abhirupsaha02@gmail.com>
2024-07-08 16:58:11 +0530

    Revert "Storage"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,32 @@
+import React from 'react'
 import Image from 'next/image';
+import { FaFacebookF, FaInstagram,FaWhatsapp,FaLinkedinIn } from 'react-icons/fa';
 import Link from 'next/link';
-import { CiGlobe } from "react-icons/ci";
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaLinkedinIn } from 'react-icons/fa';
-
-const logoStyle = "h-6 w-6 text-text_yellow hover:text-white";
-
-export default function Footer() {
+const Footer = () => {
   return (
     <div className='flex justify-between space-x-5 w-full border-t-[0.5px] border-text_yellow bottom-0 mt-20 '>
-      <div className='flex justify-start mx-4 my-2'>
-        <Image
-          src='/assets/resourciologo.png'
-          alt='logo'
-          width={180}
-          height={40}
-          unoptimized
-        />
-      </div>
-
-      <span className='font-poppins text-white my-8 justify-center'> &copy; Resourcio Community 2024</span>
-      <div className='flex gap-4 justify-end items-end ml-auto pr-10 my-8'>
+      <div className='flex justify-start'><Image src='/assets/resourciologo.png' alt='logo' width={160} height={53} className='mx-10 my-2 justify start'/></div>
+        
+        <div className='font-poppins text-white my-8 justify-center'> &copy; Resourcio Community 2024</div>
+        <div className='flex gap-4 justify-end items-end ml-auto pr-10 my-8'>
         <Link href="https://chat.whatsapp.com/Km6AX9di04ZLIpFEcXTiNK" target="_blank" rel="noopener noreferrer">
-          <FaWhatsapp className={logoStyle} />
-        </Link>
-        <Link href="https://www.instagram.com/resourciocommunity/" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className={logoStyle} />
-        </Link>
-        <Link href="https://www.facebook.com/profile.php?id=100088472180461" target="_blank" rel="noopener noreferrer">
-          <FaFacebookF className={logoStyle} />
-        </Link>
-        <Link href="https://www.linkedin.com/company/resourcio-community2022/" target="_blank" rel="noopener noreferrer">
-          <FaLinkedinIn className={logoStyle} />
-        </Link>
-        <Link href="https://www.resourcio.in/" target="_blank" rel="noopener noreferrer">
-          <CiGlobe className={logoStyle} />
-        </Link>
-      </div>
+            <FaWhatsapp className="h-6 w-6 text-text_yellow hover:text-white" />
+          </Link>
+          <Link href="https://www.instagram.com/resourciocommunity/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="h-6 w-6 text-text_yellow hover:text-white" />
+          </Link>
+          <Link href="https://www.facebook.com/profile.php?id=100088472180461" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF className="h-6 w-6 text-text_yellow hover:text-white" />
+          </Link>
+          <Link href="https://www.linkedin.com/company/resourcio-community2022/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedinIn className="h-6 w-6 text-text_yellow hover:text-white" />
+          </Link>
+          <Link href="https://www.resourcio.in/" target="_blank" rel="noopener noreferrer">
+            <Image src='/assets/web.png' alt='web' width={30} height={25} />
+          </Link>
+        </div>
     </div>
   )
 }
+
+export default Footer
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
 "use client"
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useAuthContext } from "@/context/AuthContext";
+import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-  const router = useRouter();
-  const { signInWithGoogle, user } = useAuthContext();
-
+  const router = useRouter()
+  const { signInWithGoogle, user, logOut } = useAuthContext();
+  const [photo, setPhoto] = useState("")
+  const path = usePathname()
+  useEffect(() => {
+    if (user) {
+      setPhoto(user.photoURL || "")
+    }
+  }, [user || path]);
   const handleGoogleSignIn = async () => {
-    const result = await signInWithGoogle();
-    if (result === undefined) return;
+    await signInWithGoogle();
     router.push("/");
   };
 
@@ -42,9 +49,10 @@ export default function Navbar() {
       </div>
       <div className=" flex items-center">
         {
-          user ?
-            <Image src={user.photoURL || ''} width={39} height={39} alt="user" className="rounded-full" unoptimized />
+          user?
+            <Image src={photo} width={39} height={39} alt="user" className="rounded-full"></Image>
             :
+            // 
             <button
               type="button"
               onClick={handleGoogleSignIn}
@@ -52,6 +60,14 @@ export default function Navbar() {
             >
               Sign in with Google
             </button>
+          //   <div className="flex gap-5 py-7 ">
+          //   <Link href='/signup' className="bg-transparent  text-white px-10 py-3 h-fit  font-Poppins align-middle  flex justify-center border-[0.5px] border-text_yellow text-nowrap">
+          //     Register
+          //   </Link>
+          //   <Link href='/signin' className="bg-text_yellow  text-black px-10 py-3 h-fit  font-Poppins align-middle  flex justify-center border-none text-nowrap">
+          //     Login
+          //   </Link>
+          // </div>
         }
       </div>
     </div>
