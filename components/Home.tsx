1de6a0ecd353a4f49b914e076b28b7a9f0bd3a0e
Sayan67 <sayan.das.21@aot.edu.in>
2024-07-08 16:58:33 +0530

    Homepage responsive, footer responsive, setup svgr

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,21 +1,40 @@
 import React from 'react'
 import Image from 'next/image';
+import ArrowDotted from '@/public/assets/arrow.svg'
+
 const Home: React.FC = () => {
   return (
-    <div className='flex w-full flex-col pt-28'>
-      <div className='flex font-poppins text-white text-sm md:text-xl items-center justify-center pt-20 font-light tracking-wider'>
+    <div className='flex w-full flex-col py-28'>
+      <div className='flex font-poppins text-white text-sm md:text-xl items-center justify-center pt-20 font-light tracking-widest'>
         A photography & reel making contest
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <div className='flex font-Cinzel_Decorative text-text_yellow text-[8vmax] items-center justify-center font-bold tracking-widest'>
+        <div className='flex font-Cinzel_Decorative text-text_yellow text-[7vmax] items-center justify-center font-bold tracking-widest'>
           XPOSURE
         </div>
         <div className='flex justify-between animate-fade-up animate-delay-500 animate-ease-out'>
-          <Image src='/assets/camera.svg' alt='hero' width={434} height={576} className='z-10 -mt-16 w-[22vmax]' />
+          <Image src='/assets/camera.svg' alt='hero' width={435} height={576} className='z-10 -mt-10 lg:-mt-20 w-[28vmax]' />
         </div>
-
       </div>
-
+      <div className='absolute top-[100vh] -translate-y-[110%] left-10 text-sm md:text-base'>
+        <div className='border-l border-dashed border-text_yellow h-20 hidden md:flex'></div>
+        <div className='border-l border-text_yellow h-20 flex flex-col justify-end pl-3 gap-4'>
+          <h1 className='text-neutral-200'>Unleash your creative vision</h1>
+          <div className='md:border-b md:border-text_yellow flex w-fit items-center pt-2 md:hidden'>
+            <a href='/gallery' className='text-neutral-200 flex items-center gap-2 pb-2 justify-center'>
+              Gallery
+              <ArrowDotted />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='absolute top-[100vh] -translate-y-[110%] w-fit right-10 hidden md:flex text-sm md:text-base'>
+        <div className='md:border-b md:border-text_yellow h-20 flex items-end pl-3 w-fit'>
+          <a href='/gallery' className='text-neutral-200 flex items-center gap-2 pb-2 justify-center'>Gallery
+            <ArrowDotted />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
     if (user) {
       setPhoto(user.photoURL || "")
     }
-  }, [user || path]);
+  }, [user, path]);
   const handleGoogleSignIn = async () => {
     await signInWithGoogle();
     router.push("/");
@@ -27,7 +27,7 @@ export default function Navbar() {
         <Image src="/assets/logo.svg" alt="logo" width={60} height={60} />
       </div>
 
-      <div className="flex gap-7 text-base font-poppins font-light text-white px-10 items-center">
+      <div className=" gap-7 text-sm font-poppins font-light text-white px-10 items-center tracking-wide md:flex hidden">
         <Link href="/" className="hover:text-text_yellow">
           Home
         </Link>
@@ -49,25 +49,27 @@ export default function Navbar() {
       </div>
       <div className=" flex items-center">
         {
-          user?
+          user?.photoURL?
             <Image src={photo} width={39} height={39} alt="user" className="rounded-full"></Image>
             :
             // 
             <button
               type="button"
               onClick={handleGoogleSignIn}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium text-black bg-text_yellow hover:bg-text_yellow/80 duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium text-black bg-text_yellow hover:bg-text_yellow/80 duration-300"
             >
-              Sign in with Google
+              <h1 className=" hidden md:flex">
+                Sign in with Google
+              </h1>
+              <div className="md:hidden flex gap-2 items-center">
+                <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.56 5.1C11.2 4.40667 10.7 3.88667 10.06 3.54C9.42 3.18 8.68 3 7.84 3C6.92 3 6.1 3.20667 5.38 3.62C4.66 4.03333 4.09333 4.62 3.68 5.38C3.28 6.14 3.08 7.02 3.08 8.02C3.08 9.02 3.28 9.90667 3.68 10.68C4.09333 11.44 4.66 12.0267 5.38 12.44C6.1 12.8533 6.92 13.06 7.84 13.06C9.08 13.06 10.0867 12.7133 10.86 12.02C11.6333 11.3267 12.1067 10.3867 12.28 9.2H7.06V7.38H14.72V9.16C14.5733 10.24 14.1867 11.2333 13.56 12.14C12.9467 13.0467 12.14 13.7733 11.14 14.32C10.1533 14.8533 9.05333 15.12 7.84 15.12C6.53333 15.12 5.34 14.82 4.26 14.22C3.18 13.6067 2.32 12.76 1.68 11.68C1.05333 10.6 0.74 9.38 0.74 8.02C0.74 6.66 1.05333 5.44 1.68 4.36C2.32 3.28 3.18 2.44 4.26 1.84C5.35333 1.22667 6.54667 0.919999 7.84 0.919999C9.32 0.919999 10.6333 1.28667 11.78 2.02C12.94 2.74 13.78 3.76667 14.3 5.1H11.56Z" fill="#020202" />
+                </svg>
+
+                <h1>Sign in</h1>
+              </div>
+
             </button>
-          //   <div className="flex gap-5 py-7 ">
-          //   <Link href='/signup' className="bg-transparent  text-white px-10 py-3 h-fit  font-Poppins align-middle  flex justify-center border-[0.5px] border-text_yellow text-nowrap">
-          //     Register
-          //   </Link>
-          //   <Link href='/signin' className="bg-text_yellow  text-black px-10 py-3 h-fit  font-Poppins align-middle  flex justify-center border-none text-nowrap">
-          //     Login
-          //   </Link>
-          // </div>
         }
       </div>
     </div>
