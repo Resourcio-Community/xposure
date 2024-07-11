a8e7266e262bb5fb6ce70deff115849f2fc6e0e2
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-07-11 11:24:02 +0530

    Added Terms and Condition and Shipping Policy

---
+++ app/shipping-policy/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="text-white/60 pt-28 px-[10%] space-y-3 pb-16 min-h-[80vh]">
+      <h1 className="text-[2.25vmax] font-bold">Shipping & Delivery Policy</h1>
+      <p>Last updated on Jul 10th 2024</p>
+      <p>Shipping is not applicable for business.</p>
+    </div>
+  );
+};
+
+export default page;
