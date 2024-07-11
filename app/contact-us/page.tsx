a013cbffbd367a819cedb75007bab0cc8d084297
Sayan67 <sayan.das.21@aot.edu.in>
2024-07-11 11:51:13 +0530

    Add shipping and contact page

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='space-y-3 text-white/70 pt-28 px-[10%] min-h-[80vh]'>
+            <h1 className='text-[2.25vmax] font-bold'>
+                Contact us
+            </h1>
+
+            <p className='text-sm md:text-base'>
+                Last updated on Jul 10th 2024
+            </p>
+
+            <p className='text-sm md:text-base'>
+                You may contact us using the information below:
+            </p>
+
+            <p className='text-sm md:text-base'>
+                Merchant Legal entity name: Resourcio Community
+                Registered Address: Satgachia,purba bardhaman Purba Bardhaman WEST BENGAL 713422
+                Operational Address: Satgachia,purba bardhaman Purba Bardhaman WEST BENGAL 713422
+                Telephone No:
+            </p>
+
+            <p className='text-sm md:text-base'>
+                E-Mail ID: mondalsoumya02@gmail.com
+            </p>
+        </div>
+    )
+}
+
+export default page
---
+++ app/refund-policy/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='px-[10%] pt-28 min-h-[80vh] text-white/70'>
+        <h1 className='text-[2.25vmax] font-bold'>Cancellation & Refund Policy</h1>
+        <p className='text-sm md:text-base'>Last updated on Jul 10th 2024</p>
+        <p className='text-sm md:text-base'>No cancellations & Refunds are entertained.</p>
+    </div>
+  )
+}
+
+export default page
