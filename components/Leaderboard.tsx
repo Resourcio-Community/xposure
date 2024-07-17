7ea0430727d2298e2887beea20477e70ea738bcf
ABHIRUP <abhirupsaha02@gmail.com>
2024-07-17 11:38:09 +0530

    .

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -1,16 +1,16 @@
-async function fetchLeaderboard() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/leaderboard`, {
-        next: {
-            revalidate: 3600
-        }
-    })
+// async function fetchLeaderboard() {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/leaderboard`, {
+//         next: {
+//             revalidate: 3600
+//         }
+//     })
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
+//     if (!res.ok) {
+//         throw new Error('Failed to fetch data')
+//     }
 
-    return res.json()
-}
+//     return res.json()
+// }
 
 interface ILeaderBoard {
     email: string;
@@ -19,17 +19,17 @@ interface ILeaderBoard {
 }
 
 export default async function Leaderboard() {
-    const data: Array<ILeaderBoard> = await fetchLeaderboard()
+    // const data: Array<ILeaderBoard> = await fetchLeaderboard()
 
     return (
         <div className="flex flex-col">
-            {data.map((item, idx) => (
+            {/* {data.map((item, idx) => (
                 <div key={idx} className="flex gap-4">
                     <span>{item.email}</span>
                     <span>{item.imageCount}</span>
                     <span>{item.reelCount}</span>
                 </div>
-            ))}
+            ))} */}
         </div>
     )
 }
