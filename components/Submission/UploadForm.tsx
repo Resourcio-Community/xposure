44d6ec1f685a5bff095dc09ae9156622dbcc23f1
Sayan67 <sayan.das.21@aot.edu.in>
2024-07-21 02:07:16 +0530

    matha pod sob mere gelo

--- components/Submission/UploadForm.tsx
+++ components/Submission/UploadForm.tsx
@@ -3,12 +3,9 @@ import { useAuthContext } from '@/context/AuthContext';
 import { getImageReelCountForAnUser } from '@/lib/actions/user.action';
 import { FormDataObject } from '@/types';
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { imageObjectFormat } from '@/utils/imageObjectFormat';
+import { finalObjectFormat, imageObjectFormat } from '@/utils/imageObjectFormat';
 import { imageRef } from '@/lib/firebase/storage';
 import { UploadMetadata, getDownloadURL, uploadBytesResumable, getMetadata } from 'firebase/storage';
-
-
-
 
 interface denajakhusi { metadata: string, url: string }
 
@@ -29,12 +26,12 @@ export default function UploadForm() {
     !authLoading && imageCount()
   }, [authLoading])
 
-
+  // const [uploadUrls, setUploadUrls] = useState<denajakhusi[]>([{ metadata: "", url: "" }])
 
   const [formData, setFormData] = useState<FormDataObject>({
-    section1: { section: "", image: '', category: '', theme: '' },
-    section2: { section: "", image: '', category: '', theme: '' },
-    section3: { section: "", image: '', category: '', theme: '' },
+    section1: { section: null, image: null, category: null, theme: null },
+    section2: { section: null, image: null, category: null, theme: null },
+    section3: { section: null, image: null, category: null, theme: null },
   });
 
 
@@ -58,140 +55,137 @@ export default function UploadForm() {
           ...formData[sectionNum],
           section: name,
           image: file,
-
         },
       });
     }
   };
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const arr = imageObjectFormat(formData);
-    const downloadUrls: denajakhusi[] = upload(arr)
-  //   downloadUrls.map((item) => {
-  //     console.log(item);
-  //     const key = item.metadata as keyof FormDataObject;
-  //     setFormData(prev => (
-
-  //       { ...prev ,
-  //         [key]: {...prev[key], url: item.url}
-  //       }
-  //     ) 
-  //   )
-  // })
-    console.log('Merged Data:', downloadUrls);
-};
-
-
-const upload: any = (items: any) => {
-  let imageArr: denajakhusi[] = []
-  items.forEach((item: any) => {
-    const metaData: UploadMetadata = { customMetadata: { name: item.label } };
-    const fileName = new Date().getTime().toString();
-    if (user && user.email) {
-      const storageRef = imageRef(user.email, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, item.file, metaData);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(progress);
-        },
-        (err) => {
-          console.log(err);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            getMetadata(uploadTask.snapshot.ref).then(metaData => {
-              imageArr.push({ metadata: metaData.customMetadata?.name || "", url: downloadURL });
-              // console.log("File available at", metaData);
-            })
-          });
-
-        }
-      );
+    const downloadUrls = await upload(arr);
+    const updatedFormData = { ...formData };
+    downloadUrls.forEach((item) => {
+      const key = item.metadata as keyof FormDataObject;
+      updatedFormData[key] = {
+        ...updatedFormData[key],
+        url: item.url
+      };
+    });
+    if (user?.name && user.email && user.photoURL) {
+      const res = finalObjectFormat(updatedFormData, user?.name, user?.email, user?.photoURL)
+      console.log(res);
     }
-  });
-  return imageArr;
-};
+    // setFormData(updatedFormData);
+    console.log('Merged Data:', updatedFormData);
+  };
 
+  const upload = async (items: any) => {
+    const imageArr: denajakhusi[] = [];
+    const promises = items.map((item: any) => {
+      const metaData: UploadMetadata = { customMetadata: { name: item.label } };
+      const fileName = new Date().getTime().toString();
+      if (user && user.email) {
+        const storageRef = imageRef(user.email, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, item.file, metaData);
 
-return (
-  <div className=' pt-10 space-y-12'>
-    <h1 className='text-3xl font-semibold text-text_yellow'>You can upload {3 - photoCount} more photo!</h1>
-    <form onSubmit={handleSubmit} className='flex gap-16 flex-col items-center'>
-      <div className='flex gap-12 items-center justify-center flex-wrap'>
-        {[1, 2, 3].map((section) => (
-          <div key={section} className={`${section <= photoCount && "opacity-30"} flex flex-col items-center gap-4`}>
-            <h3>Section {section}</h3>
+        return new Promise<void>((resolve, reject) => {
+          uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+              const progress =
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log(progress);
+            },
+            (err) => {
+              console.log(err);
+              reject(err);
+            },
+            async () => {
+              try {
+                const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                const metaData = await getMetadata(uploadTask.snapshot.ref);
+                imageArr.push({ metadata: metaData.customMetadata?.name || "", url: downloadURL });
+                resolve();
+              } catch (err) {
+                reject(err);
+              }
+            }
+          );
+        });
+      }
+    });
+    await Promise.all(promises);
+    return imageArr;
+  };
 
-            <div className="flex items-center justify-center w-[22rem]">
-              <label htmlFor={`section${section}`} className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                  <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                  </svg>
-                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                  <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG or JPEG</p>
+  return (
+    <div className=' pt-10 space-y-12'>
+      <h1 className='text-3xl font-semibold text-text_yellow'>You can upload {3 - photoCount} more photo!</h1>
+      <form onSubmit={handleSubmit} className='flex gap-16 flex-col items-center'>
+        <div className='flex gap-12 items-center justify-center flex-wrap'>
+          {[1, 2, 3].map((section) => (
+            <div key={section} className={`${section <= photoCount && "opacity-30"} flex flex-col items-center gap-4`}>
+              <h3>Section {section}</h3>
+
+              <div className="flex items-center justify-center w-[22rem]">
+                <label htmlFor={`section${section}`} className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">PNG, JPG or JPEG</p>
+                  </div>
+                  <input
+                    disabled={section <= photoCount}
+                    accept='image/*'
+                    id={`section${section}`} type="file"
+                    name={`section`}
+                    onChange={(e) => handleFileChange(e, `section${section}` as keyof FormDataObject, e.target.id)} className="hidden disabled:"
+                  />
+                </label>
+              </div>
+
+              <div className='flex gap-6'>
+                <div className='flex gap-2'>
+                  <input
+                    disabled={section <= photoCount}
+                    type="radio"
+                    name={`category`}
+                    value="mobile"
+                    onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
+                  />
+                  <h1>Mobile</h1>
                 </div>
-                <input
-                  disabled={section <= photoCount}
-                  accept='image/*'
-                  id={`section${section}`} type="file"
-                  name={`section`}
-                  onChange={(e) => handleFileChange(e, `section${section}` as keyof FormDataObject, e.target.id)} className="hidden disabled:"
-                />
-              </label>
-            </div>
 
-            <div className='flex gap-6'>
-
-              <div className='flex gap-2'>
-                <input
-                  disabled={section <= photoCount}
-                  type="radio"
-                  name={`category`}
-                  value="mobile"
-                  onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
-                />
-                <h1>
-                  Mobile
-                </h1>
+                <div className='flex gap-2'>
+                  <input
+                    disabled={section <= photoCount}
+                    type="radio"
+                    name={`category`}
+                    value="camera"
+                    onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
+                  />
+                  <h1>Camera</h1>
+                </div>
               </div>
 
-              <div className='flex gap-2'>
-                <input
-                  disabled={section <= photoCount}
-                  type="radio"
-                  name={`category`}
-                  value="camera"
-                  onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
-                />
-                <h1>
-                  Camera
-                </h1>
-              </div>
+              <select
+                disabled={section <= photoCount}
+                name="theme"
+                onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
+              >
+                <option value="">Select an category</option>
+                <option value="option1">Nature in night</option>
+                <option value="option2">Travel and tourist photography</option>
+                <option value="option3">Tech in nature</option>
+              </select>
             </div>
-
-            <select
-              disabled={section <= photoCount}
-              name="theme"
-              onChange={(e) => handleChange(e, `section${section}` as keyof FormDataObject)}
-            >
-              <option value="">Select an category</option>
-              <option value="option1">Nature in night</option>
-              <option value="option2">Travel and tourist photography</option>
-              <option value="option3">Tech in nature</option>
-            </select>
-
-
-          </div>
-        ))}
-      </div>
-      <button type="submit" className={`bg-text_yellow w-fit text-black px-6 py-1 hover:bg-text_yellow/80 duration-300`}>Submit</button>
-    </form>
-  </div>
-);
+          ))}
+        </div>
+        <button type="submit" className={`bg-text_yellow w-fit text-black px-6 py-1 hover:bg-text_yellow/80 duration-300`}>Submit</button>
+      </form>
+    </div>
+  );
 };
--- utils/imageObjectFormat.ts
+++ utils/imageObjectFormat.ts
@@ -1,33 +1,43 @@
+import { DBUser, FormDataObject } from "@/types";
 
-import { FormDataObject } from "@/types";
-
-
-export function imageObjectFormat(data:FormDataObject){
-    let imageArray:any=[];
-    Object.values(data).map((item)=>{
-        const tempArr = Object.values(item)
-        if(tempArr.some((elem)=>elem!=='')){
-                imageArray.push({label:item.section,file:item.image})
-        }
-        
+export function imageObjectFormat(data: FormDataObject) {
+  let imageArray: any = [];
+  Object.values(data).map((item) => {
+    const tempArr = Object.values(item);
+    if (tempArr.some((elem) => elem !== "")) {
+      imageArray.push({ label: item.section, file: item.image });
     }
-    )
-    console.log(imageArray);
+  });
 
-    // console.log(data);
-    return imageArray;   
+  return imageArray;
 }
 
-// const handleUpload = (e: MouseEvent) => {
-//     e.preventDefault();
-//     if (images === undefined) return;
-//     let imageArr: any = [];
-//     Object.entries(images).forEach(([key, val]) => {
-//       imageArr.push({ label: key, file: val });
-//     });
-//     upload(imageArr);
-//   };
-
-//   const handleSubmit = (e: FormEvent) => {
-//     e.preventDefault();
-//   };
+export function finalObjectFormat(
+  data: FormDataObject,
+  name: string,
+  email: string,
+  photoURL: string
+) {
+  let finalObject: DBUser = {
+    name,
+    email,
+    photoURL,
+    img_1: data.section1.url as string,
+    imgCategory_1: data.section1.category!,
+    imgTheme_1: data.section1.theme!,
+    img_2: data.section2.url!,
+    imgCategory_2: data.section2.category!,
+    imgTheme_2: data.section2.theme!,
+    img_3: data.section3.url!,
+    imgCategory_3: data.section3.category!,
+    imgTheme_3: data.section3.theme!,
+    reel_1: null!,
+    reelCategory_1: null!,
+    reelTheme_1: null!,
+    reel_2: null!,
+    reelCategory_2: null!,
+    reelTheme_2: null!,
+    txn: "ABCDEFGH",
+  }
+  return finalObject;
+}
