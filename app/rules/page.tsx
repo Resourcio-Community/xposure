270e44d6f2bdcd0d6e91209a07a3298ff343e73f
Sayan67 <sayan.das.21@aot.edu.in>
2024-07-26 11:34:56 +0530

    Update rules

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -13,25 +13,22 @@ export default function Rules() {
 
       <div className="flex space-x-8 md:text-lg justify-center">
         <button
-          className={`${
-            activeTab === "themes" ? "border-b-4 border-text_yellow" : ""
-          } pb-2 hover:text-text_yellow`}
+          className={`${activeTab === "themes" ? "border-b-4 border-text_yellow" : ""
+            } pb-2 hover:text-text_yellow`}
           onClick={() => setActiveTab("themes")}
         >
           Themes
         </button>
         <button
-          className={`${
-            activeTab === "photography" ? "border-b-4 border-text_yellow" : ""
-          } pb-2 hover:text-text_yellow`}
+          className={`${activeTab === "photography" ? "border-b-4 border-text_yellow" : ""
+            } pb-2 hover:text-text_yellow`}
           onClick={() => setActiveTab("photography")}
         >
           Photography Guidelines
         </button>
         <button
-          className={`${
-            activeTab === "reels" ? "border-b-4 border-text_yellow" : ""
-          } pb-2 hover:text-text_yellow`}
+          className={`${activeTab === "reels" ? "border-b-4 border-text_yellow" : ""
+            } pb-2 hover:text-text_yellow`}
           onClick={() => setActiveTab("reels")}
         >
           Shorts Guidelines
@@ -44,31 +41,24 @@ export default function Rules() {
           <h3 className="text-xl font-semibold">Photography</h3>
           <ul className="list-decimal list-inside space-y-2 ml-4">
             <li>
-              <b>Nature in Night</b>: Capturing the picturesque view of nature
-              in the night.
+              <b>Nature in Night</b>: Capturing the picturesque view of nature in the night.
             </li>
             <li>
-              <b>Travel & Tourist Photography</b>: Capturing memories through
-              your Travelling and Tourism.
+              <b>Travel & Tourist Photography</b>: Capturing memories through your Travelling and Tourism.
             </li>
             <li>
-              <b>Tech in nature</b>: Capture the intersection of technology and
-              the natural world.
+              <b>Tech in Nature</b>: Capture the intersection of technology and the natural world.
             </li>
           </ul>
+
+
           <h3 className="text-xl font-semibold mt-4">Shorts</h3>
           <ul className="list-decimal list-inside space-y-2 ml-4">
             <li>
-              <b>Explore your city/ village</b>: Creating Shorts about your
-              special moments with your city life.
+              <b>Explore Your City/Village</b>: Creating reels about your special moments with your city life.
             </li>
             <li>
-              <b>B roll of any Nature Element - Cinematic Shot</b>: The term
-              B-roll originates from the early days of film, when editors
-              inserted supplemental footage or B-roll, into the main footage or
-              A-roll, to hide visible lines where two pieces of film were
-              joined. In modern film and video production, B-roll describes all
-              of the footage that isn&apos;t the main action.
+              <b>A Viewport to Your Daily Life</b>: Shoot your daily life inside a reel.
             </li>
           </ul>
         </div>
@@ -81,23 +71,16 @@ export default function Rules() {
           </h2>
           <ol className="list-decimal list-inside space-y-2 w-[80%]">
             <li>
-              Categories: <b>Mobile Photography, Camera Photography.</b>
+              Dimensions: <b>3:4, 3:2, 2:3, 4:3</b>
             </li>
             <li>
-              Photo Dimensions: <b>3:4, 3:2, 2:3, or 4:3.</b>
+              Max 3 photos per participant
             </li>
             <li>
-              <b>Watermark Requirement</b>: Mandatory for all photographs.
-            </li>
-            <li>
-              Entry Fee:
+              Entry Fees:
               <ul className="space-y-2 list-inside ml-6">
                 <li>
-                  Mobile & Camera Photography (Max 3 photos, total fee: Rs. 50):
-                  <ul
-                    className="space-y-1 list-inside ml-6"
-                    style={{ listStyleType: "lower-roman" }}
-                  >
+                  <ul className="space-y-1 list-inside ml-6" style={{ listStyleType: "lower-roman" }}>
                     <li>1st Photo: Rs. 25</li>
                     <li>2nd Photo: Rs. 15</li>
                     <li>3rd Photo: Rs. 10</li>
@@ -106,97 +89,33 @@ export default function Rules() {
               </ul>
             </li>
             <li>
-              <b>Photo Enhancement</b>: Allowed without altering the real
-              content. No manipulation; disqualification if detected.
+              <b>Enhancement Allowed</b>: No manipulation
             </li>
             <li>
-              <b>Authenticity</b>: Only original works. No plagiarism.
+              <b>Must Be Authentic Work</b>
             </li>
             <li>
-              <b>Device Information</b>: Mention device name and model in the
-              caption.
+              <b>No AI-generated Content</b>
             </li>
             <li>
-              <b>No AI-generated Works</b>: Not accepted.
+              Highest Pixels, <b>JPEG Format</b>
             </li>
             <li>
-              <b>Submission</b>: Normal submission will be in the website{" "}
-              <b>Submission Form</b>. For Viewer&apos;s Choice Award
-              (Photography only) you have to upload on Instagram as well.
-            </li>
-            <li>
-              <b>Winners</b>: One Winner per Photography theme, selected by
-              judges.
-            </li>
-            <li>
-              <b>Viewer&apos;s Choice Award and Eligibility Criteria:</b>
-              <ul
-                className="space-y-1 list-inside ml-6"
-                style={{ listStyleType: "lower-alpha" }}
-              >
-                <li>Photo must have a watermark.</li>
+              <b>Viewer’s Choice Award</b>:
+              <ul className="space-y-1 list-inside ml-6" style={{ listStyleType: "lower-alpha" }}>
+                <li>Optional watermark</li>
                 <li>
-                  Tag Resourcio Community on social media (
-                  <Link
-                    href="https://linktr.ee/resourciocommunity22"
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Link Tree(social media links)
-                  </Link>
-                  ).
+                  Post photo on social media tagging Resourcio Community -
+                  <a href="https://linktr.ee/resourciocommunity22" className="underline" target="_blank" rel="noopener noreferrer">
+                    https://linktr.ee/resourciocommunity22
+                  </a>
                 </li>
-                <li>Submit photo via Submission Form on the website.</li>
-                <li>
-                  Most reactions will win the Viewer&apos;s Choice Photo Award.
-                </li>
-                <li>
-                  Tagging Resourcio Community on Instagram (
-                  <Link
-                    href="https://www.instagram.com/resourciocommunity/"
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    @resourciocommunity
-                  </Link>
-                  ) is mandatory.
-                </li>
-                <li>
-                  Submissions only through the website&apos;s{" "}
-                  <b>Submission Form</b> will be considered eligible.
-                </li>
+                <li>Submit the same photo on the website</li>
+                <li>Most reactions win</li>
               </ul>
-            </li>
-            <li>
-              <b>Submission Format</b>: Photos must be in <b>JPEG format</b>
-              ,highest possible resolution.
-            </li>
-            <li>
-              <b>Disqualification</b>:{" "}
-              <ul
-                className="space-y-1 list-inside ml-6"
-                style={{ listStyleType: "lower-roman" }}
-              >
-                <li>
-                  Any misleading information or religious hatred symbols will
-                  lead to direct disqualification.
-                </li>
-                <li>
-                  Your photograph must respect intellectual property rights and
-                  not violate privacy or publicity rights.
-                </li>
-                <li>
-                  Your photograph must not promote illegal activities or
-                  violations of any law.
-                </li>
-              </ul>
-            </li>
-            <li>
-              Judges&apos; Decision: Final and not subject to correspondence.
             </li>
           </ol>
+
         </div>
       )}
 
@@ -205,103 +124,56 @@ export default function Rules() {
           <h2 className="text-2xl font-bold mb-4 text-text_yellow">
             Shorts Guidelines
           </h2>
-          <ol className="list-decimal list-inside space-y-2">
+          <ol className="list-decimal list-inside space-y-2 w-[80%]">
             <li>
-              Short Dimensions: <b>9:16</b>.
+              Dimensions: <b>9:16</b>
             </li>
             <li>
-              Short length: Minimum <b>30</b> seconds, maximum <b>45</b> seconds.
+              Length: <b>maximum 30 seconds</b>
             </li>
             <li>
-              <b>Watermark Requirement</b>: Mandatory for all shorts.
+              Max 2 reels per participant
             </li>
             <li>
-              Entry Fee:
+              Entry Fees:
               <ul className="space-y-2 list-inside ml-6">
                 <li>
-                  Short Making (Max 2 shorts, total fee: Rs. 60):
-                  <ul
-                    className="space-y-1 list-inside ml-6"
-                    style={{ listStyleType: "lower-roman" }}
-                  >
-                    <li>1st Short: Rs. 40</li>
-                    <li>2nd Short: Rs. 20</li>
+                  <ul className="space-y-1 list-inside ml-6" style={{ listStyleType: "lower-roman" }}>
+                    <li>1st Reel: Rs. 40</li>
+                    <li>2nd Reel: Rs. 20</li>
                   </ul>
                 </li>
               </ul>
             </li>
             <li>
-              <b>Authenticity</b>: Only original works. No plagiarism.
+              <b>Must Be Authentic Work</b>
             </li>
             <li>
-              <b>Device Information</b>: Mention device name and model in the
-              caption.
+              Mention the device name/model in the caption
             </li>
             <li>
-              <b>No AI-generated Works</b>: Not accepted.
+              <b>No AI-generated Content</b>
             </li>
             <li>
-              <b>Submission</b>:
-              <ul
-                className="space-y-1 list-inside ml-6"
-                style={{ listStyleType: "lower-roman" }}
-              >
-                <li>Short must have a watermark.</li>
-                <li>Post the short on any social media platform.</li>
-                <li>
-                  {" "}
-                  Tag Resourcio Community on social media (
-                  <Link
-                    href="https://linktr.ee/resourciocommunity22"
-                    className="underline"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Link Tree(social media links)
-                  </Link>
-                  ).
-                </li>
-                <li>
-                  Submit the posted short <b>Link</b> via Submission Form on the
-                  website.
-                </li>
-                <li>
-                  Submissions only through the website&apos;s{" "}
-                  <b>Submission Form</b> will be considered eligible.
-                </li>
-              </ul>
+              Post reel on social media tagging Resourcio Community -
+              <a href="https://linktr.ee/resourciocommunity22" className="underline" target="_blank" rel="noopener noreferrer">
+                https://linktr.ee/resourciocommunity22
+              </a>
             </li>
             <li>
-              <b>Winners</b>: One Winner per Short theme, selected by judges.
+              Submit the posted reel link on the website
             </li>
             <li>
-              <b>Submission Format</b>: Shorts must be in the highest possible
-              resolution.
+              Optional watermark
             </li>
             <li>
-              <b>Disqualification</b>:{" "}
-              <ul
-                className="space-y-1 list-inside ml-6"
-                style={{ listStyleType: "lower-roman" }}
-              >
-                <li>
-                  Any misleading information or religious hatred symbols will
-                  lead to direct disqualification.
-                </li>
-                <li>
-                  Your short must respect intellectual property rights and not
-                  violate privacy or publicity rights.
-                </li>
-                <li>
-                  Your short must not promote illegal activities or violations of
-                  any law.
-                </li>
-              </ul>
+              Can include outro branding
             </li>
             <li>
-              Judges&apos; Decision: Final and not subject to correspondence.
+              Can write the idea of making the reel in the caption
             </li>
           </ol>
+
         </div>
       )}
     </div>
